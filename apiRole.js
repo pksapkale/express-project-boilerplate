e74888e55c9bb296d20be859346932f456e83ab2
Pushpendra Sapkale <116496506+pksapkale@users.noreply.github.com>
2023-12-13 20:43:28 +0530

    Added authorization
    Added authorization

---
+++ apiRole.js
@@ -0,0 +1,5 @@
+const apiRole = {
+    "auth/re_gen_token": ['Admin', 'Superadmin']
+}
+
+module.exports = apiRole;
--- authenticateToken.js
+++ authenticateToken.js
@@ -6,24 +6,48 @@
 */
 
 const jwt = require('jsonwebtoken');
+const pool = require('./db/connection');
+const apiRole = require('./apiRole');
+
 
 const authenticateToken = (req, res, next) => {
     const token = req.header('Authorization');
+    const reqURL = req.originalUrl;
 
     if (!token) {
         return res.status(401).json({ status: false, message: 'Access denied. Token not provided.' });
     }
 
-    jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    jwt.verify(token, process.env.TOKEN_KEY, async (err, user) => {
         if (err) {
             return res.status(403).json({ status: false, message: err.message });
         }
-        // if() {
-        //     return res.status(401).json({ status: false, message: "User is not authorized to particular service" });
-        // }
+        if (!await checkTokenInDB(user, token)) {
+            return res.status(401).json({ status: false, message: "User is not authorized to particular service" });
+        }
+        if (!checkAuthorization(user.user_type, reqURL)) {
+            return res.status(401).json({ status: false, message: "User is not authorized to particular service" });
+        }
         req.user = user;
         next();
     });
+
+    async function checkTokenInDB(user, token) {
+        let query = `SELECT * FROM users WHERE user_id = ? AND user_type = ? AND token = ?`;
+        const [updateData] = await pool.query(query, [user.user_id, user.user_type, token]);
+        if (updateData && updateData.length) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    function checkAuthorization(user_type, apiURL) {
+        let apiName = apiURL.substring(1);
+        return apiRole[apiName].includes(user_type) ? true : false;
+    }
+
 };
 
 
