379fcd66e90d25c09c1ffd13b1220de87347992e
Pushpendra Sapkale <116496506+pksapkale@users.noreply.github.com>
2023-12-31 13:01:37 +0530

    Updated readme.md file
    Added instructions how to install and run this project in your local setup in readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# auth-service
-Here we have authentication and authorization according to multiple users
+
+# Express Project Boilerplate
+
+Welcome to the Express Project Boilerplate repository! This is a boilerplate of express project with  all the required security and autherization / authentications
+
+## Getting Started
+
+### Prerequisites
+
+Before you begin, ensure that you have the following installed on your machine:
+
+1. [Node.js](https://nodejs.org/) - Latest LTS Version
+2. [MySQL](https://www.mysql.com/) - Install MySQL and import the 'db.sql' file into your MySQL database. Find the 'db.sql' file in the root directory of the project.
+
+### Installation
+
+1. **Clone The Repository and open in terminal:**
+
+   ```bash
+   git clone https://github.com/pksapkale/express-project-boilerplate.git
+   cd express-project-boilerplate
+   ```
+2. **Install node packages:**
+
+   ```bash
+   npm i
+   ```
+3. **Start Server:**
+
+   ```bash
+   npm start
+   ```
---
+++ db.sql
@@ -0,0 +1,65 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for macos13 (arm64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `newo`
+--
+
+DROP TABLE IF EXISTS `newo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `newo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_first_name` varchar(45) NOT NULL,
+  `user_last_name` varchar(45) NOT NULL,
+  `user_email` varchar(45) NOT NULL,
+  `user_phone` varchar(15) DEFAULT NULL,
+  `user_type` varchar(45) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `token` text,
+  `refresh_token` text,
+  `created_on` bigint NOT NULL,
+  `modified_on` bigint NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `user_phone_UNIQUE` (`user_phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-31 12:50:22
